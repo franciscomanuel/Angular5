e62e286ea4861485f775a371210336453b1d332e
franciscomanuel <manu0301@gmail.com>
2018-04-28 09:27:22 +0200

    Cambio de editor

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+  progreso: number = 50;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  cambiarValor(valor) {
+
+    if ( this.progreso > 100 ) {
+      this.progreso = 100;
+    } else if ( this.progreso < 0 ) {
+      this.progreso = 0;
+    } else {
+      this.progreso += valor;
+    }
   }
 
 }
