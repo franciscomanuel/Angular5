4f41eda0665bd35db052438fc049ec53d3b09ba2
franciscomanuel <manu0301@gmail.com>
2018-05-15 16:47:57 +0200

    Función working para temas activa

--- src/app/pages/account-setting/account-setting.component.ts
+++ src/app/pages/account-setting/account-setting.component.ts
@@ -13,10 +13,22 @@ export class AccountSettingComponent implements OnInit {
   ngOnInit() {
   }
 
-  cambiarTema(tema: string) {
-    console.log(tema);
+  cambiarTema(tema: string, link: any) {
+
+    this.aplicarCheck(link);
+
     let url = `assets/css/colors/${ tema }.css`;
     this._document.getElementById('tema').setAttribute('href', url);
+  }
+
+  aplicarCheck(link: any) {
+    let selectores: any = document.getElementsByClassName('selector');
+
+    for(let ref of selectores) {
+      ref.classList.remove('working');
+    }
+
+    link.classList.add('working');
   }
 
 }
